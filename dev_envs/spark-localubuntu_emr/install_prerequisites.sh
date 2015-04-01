f8765efafc506140cb6395374226964adf92953f
juanrh <juan.rodriguez.hortala@gmail.com>
2015-04-01 18:04:05 +0200

    added instructions for manual install of eclipse

--- dev_envs/spark-localubuntu_emr/install_prerequisites.sh
+++ dev_envs/spark-localubuntu_emr/install_prerequisites.sh
@@ -8,8 +8,15 @@ function prepare_anaconda {
 	echo "Then use 'source /opt/anaconda/activate_anaconda.sh' to activate anaconda"
 }
 
+function prepare_eclipse_kepler {
+    echo 'Download "Eclipse IDE for Java Developers" for Linux 64 bits from http://www.eclipse.org/downloads/'
+    echo "We need Scala 2.10 for Spark, and Eclipse only supports one plugin installation for Scala at a time"
+    echo "Install Scala IDE 3.0.3 for Eclipse from http://scala-ide.org/download/prev-stable.html using the update site http://download.scala-ide.org/sdk/helium/e38/scala210/stable/site"
+}
+
 function prepare_python27 {
 	# Install pip and Fabric in the system version of python 2.7
+    # Note this Python version is different to Anaconda python
 	get_pip_url='https://bootstrap.pypa.io/get-pip.py'
 	get_pip_name=$(basename $get_pip_url)
 	pushd /tmp
@@ -24,4 +31,5 @@ function prepare_python27 {
 # Main
 ###################
 prepare_anaconda
+prepare_eclipse_kepler
 prepare_python27
